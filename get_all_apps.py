0d3e3ffe0835caeb00f33cad1b42a29481fd2253
saprykins <60004824+saprykins@users.noreply.github.com>
2023-01-05 08:59:24 +0100

    Update get_all_apps.py
    added try (to avoid errors related to empty cells) and split on batches

--- get_all_apps.py
+++ get_all_apps.py
@@ -13,7 +13,8 @@ pat = '***'
 authorization = str(base64.b64encode(bytes(':'+pat, 'ascii')), 'ascii')
 
 # initialization dataFrame
-cols =  ["wi_id", "wi_title", "wi_parent_id", "wi_state"]
+# cols =  ["wi_id", "wi_title", "wi_parent_id", "wi_state"]
+cols =  ["id", "title", "parent_id", "state", "project", "backlog", "type", "assigned_to", "created", "changed"]
 df_applications = pd.DataFrame([],  columns = cols)
 
 
@@ -40,6 +41,30 @@ def save_working_item_into_data_frame(working_item_id, df_applications):
 
     # working item State
     wi_state = response.json()["fields"]["System.State"]
+    
+    try:
+        wi_project = response.json()["fields"]["System.TeamProject"] # Mig Factory
+    except: 
+        wi_project = ""
+
+    try:
+        wi_backlog = response.json()["fields"]["System.AreaLevel2"] # AFA
+    except: 
+        wi_backlog = ""
+    wi_type = response.json()["fields"]["System.WorkItemType"] # application, task
+    
+    # not all the tasks have assigmnment person
+    try:
+        wi_assignment = response.json()["fields"]["System.AssignedTo"]["displayName"] # mr.david
+    except:
+        wi_assignment = ""
+
+    wi_created_date = response.json()["fields"]["System.CreatedDate"]
+    
+    try:
+        wi_changed_date = response.json()["fields"]["System.ChangedDate"] # mr.david
+    except:
+        wi_changed_date = ""
 
     # working item Relations
     relations = response.json()["relations"]
@@ -50,7 +75,7 @@ def save_working_item_into_data_frame(working_item_id, df_applications):
             start_line = raw_id.find('workItems/') + 10
             parent_id = int(raw_id[start_line:])
 
-    new_row = [working_item_id, wi_title, parent_id, wi_state]
+    new_row = [working_item_id, wi_title, parent_id, wi_state, wi_project, wi_backlog, wi_type, wi_assignment, wi_created_date, wi_changed_date]
     new_df = pd.DataFrame([new_row], columns=cols)
     
     # load data into a DataFrame object:
@@ -111,23 +136,42 @@ applications = response.json()["workItems"]
 
 # n is number of applications
 # scrawling one application takes about 25 seconds
-n = 2 
+n = 10
+# first batch
 applications = applications[:n]
+
+# last
+# applications = applications[12*n:]
+
 
 # go through all(n) the apps
 for i in range(len(applications)):
     application_id = applications[i]["id"]
     feature_ids = show_children(application_id)
+
+    # store each application
+    df_applications = save_working_item_into_data_frame(application_id, df_applications)
+
     # go through childs of application (is feature)
     for feature_id in feature_ids:
+
+        # store each feature
+        df_applications = save_working_item_into_data_frame(feature_id, df_applications)
+
         user_stories = show_children(feature_id)
+    
         # go through childs of features (user_stories
         for user_story in user_stories:
+            # store each user story
+            df_applications = save_working_item_into_data_frame(user_story, df_applications)
+
             tasks = show_children(user_story)
+    
             # go through childs of user_stories (tasks)
             for task in tasks:
                 df_applications = save_working_item_into_data_frame(task, df_applications)
+
     # print(df_applications)
 
 # saves to csv file
-df_applications.to_csv('file_name_2.csv')
+df_applications.to_csv('file_name_140x.csv')
